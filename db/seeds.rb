84d1471a371fb9b2c524adfa4f0311ff2a6a8427
bouboulahcen@gmail.com <bouboulahcen@gmail.com>
2024-02-04 16:02:13 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+restaurants_data = [
+  { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" },
+  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" },
+  { name: "La Cosa mia", address: "123 Some St, London W1 4AB", category: "japanese" },
+  { name: "Five Guys", address: "789 Another St, London SE3 5CD", category: "french" },
+  { name: "Tibittis", address: "456 Different St, London NW2 6EF", category: "belgian" }
+]
+restaurants_data.each do |restaurant_data|
+  Restaurant.create!(restaurant_data)
+end
+puts "Finished!"
